71a90c132bf0f598802c038f4302c8c7826c2997
stardust <lql9705@gmail.com>
2023-09-03 16:11:40 +0800

    WIP: vm

--- include/aa64reg.h
+++ include/aa64reg.h
@@ -9,7 +9,7 @@
 #define T1_CACHE_ATTR  ((1 << 24))  /* Inner Write-Back Read-Allocate Write-Allocate Cacheable. */
 #define T1SZ  ((64 - VA_BITS) << 16)
 
-#define TG0 (0)   /* 4KB */
+#define TG0 (0)        /* 4KB */
 #define T0_CACHE_ATTR ((1 << 8))  /* Inner Write-Back Read-Allocate Write-Allocate Cacheable. */
 #define T0SZ  ((64 - VA_BITS) << 16)
 
@@ -20,5 +20,32 @@
 
 #define	TCR_IPS_SHIFT		32
 #define	TCR_IPS_WIDTH		3
+
+#define	TD_PAGE	       0x3
+#define	TD_BLOCK       0x1
+#define	TD_TABLE       0x3
+#define	TD_VALID       0x1
+
+#define TD_ATTR_SH_INNER       (3 << 8)     /* Inner Shareable */
+#define	TD_ATTR_AF		  (1 << 10)    /* Access flag */
+#define TD_ATTR_nG             (1 << 11)    /* non-global */ 
+#define	TD_DEFAUL_ATTR  (TD_ATTR_AF | TD_ATTR_nG | TD_ATTR_SH_INNER | MAIR_INDEX_WB << 2)
+
+#define	TD_ATTR_UXN  (1 << 54)       /* Unprivileged Execute Never */
+
+#define	TD_TATTR_UXN  (1 << 60)     /* descriptor Unprivileged Execute Never */
+#define	TD_TATTR_UAN  (1 << 61)     /* Unprivileged access not permitted. */
+#define TD_TATTR_DEFAULT (TD_TATTR_UXN | TD_TATTR_UAN)
+
+#define L0_SHIFT   39
+
+#define L1_SHIFT   30
+#define L2_SHIFT   21
+#define L3_SHIFT   12
+
+#define Ln_ENTRIES_SFHIT 9
+#define Ln_ENTRIES (1 << Ln_ENTRIES_SFHIT)
+#define Ln_ADDR_MASK (Ln_ENTRIES - 1)
+#define Ln_TABLE_MASK ((1 << 12) - 1)
 
 #endif
